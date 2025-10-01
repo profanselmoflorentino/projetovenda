bcd6cf024f4e8ccb4e27d2ce6096149855e25493
Anselmo Florentino <32046841+profanselmoflorentino@users.noreply.github.com>
2025-10-01 14:23:02 -0300

    Primeira Versão
    Em desenvolvimento

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="corpo">
+       <form action="imc.php" method="POST">
+          Nome: 
+          <input type="text" name="cxnome"><br/>
+          Altura:
+          <input type="text" name="cxaltura"><br/>
+          Peso:
+          <input type="text" name="cxpeso"><br/>
+          <input type="submit" value="IMC">
+     </form> 
+     </div>    
+</body>
+</html>
